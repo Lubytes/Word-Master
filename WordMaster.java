8b1a9814e1b0ea43d2f4e0a22600e0b589164627
Raviex <raistlin.hume@gmail.com>
2014-11-22 19:00:17 -0400

    Copied over all changes I made

--- WordMaster.java
+++ WordMaster.java
@@ -9,16 +9,16 @@ public class WordMaster {
 		Scanner kb = new Scanner(System.in);
 		//a = anagrams, b = punctuation, c = homonyms
 		int a = 1, b = 2, c = 3;
-		
+
 		//Introduction
-		System.out.println("Hello user! What is your name?");
+		System.out.println("Hello user! What is your name? ");
 		String name = kb.nextLine();
 		System.out.println("Well, " + name + " we're going to go over some basic English skills. "
 				+ "\nWould you like to start with anagrams(1), punctuation(2), or homonyms(3)?"
 				+ "\nPlease type the number that corresponds with the section."
 				+ "\nRemember that you may not do a section more than once.");
 		int s = kb.nextInt();
-		
+
 		//Section 1
 		if (s == a){
 			int score1 = anag();
@@ -36,61 +36,63 @@ public class WordMaster {
 			System.out.println("You have already completed that section");
 
 	}
+
 	public static int anag(){
 		//Have a word vault, scramble words using a scrambler we design. (input and check if String equals)
-			
-			Scanner kb = new Scanner(System.in);
-			Random rnd = new Random();
-			int i = 0;
-			String ans;
-			int score1 = 0;
-			boolean b;
-			int [] test = new int[10];
 
-			String [] word = {"hello", "goodbye", "greetings", "helpful", "cheese", "amazing", "question", "forever"
-								+ "remote", "board", "white", "black", "colour", "green", "yellow", "orange", "fun" 
-								+ "one", "two", "three", "four", "five", "six", "seven", "eight", "excellent"};
-			
-			System.out.print("Welcome to the anagram section! An anagram is a word that has its letters in a random order."
-			+ "\nYour job is to state the correct order of the word."
-			+ "\nCapitals are not used, so don't worry about having a lowercase at the beginning of a word! ");
-			
-			//counts 10 questions
-			for (int z = 0; z<10; z++){
-				b = false;
-				//Makes sure words are not re-used
-				while(b == false){
-					b = true;
-					i = rnd.nextInt(25);//# of words-1
-					for(int y = 0; y<10; y++){
-						if(i == (test[y])){
-							b = false;
-						}
+		Scanner kb = new Scanner(System.in);
+		Random rnd = new Random();
+		int i = 0;
+		String ans;
+		int score1 = 0;
+		boolean b;
+		int [] test = new int[10];
+
+		//If any words are added the random num (i) needs to be changed
+		String [] word = {"hello", "goodbye", "greetings", "helpful", "cheese", "amazing", "question", "forever"
+				+ "remote", "board", "white", "black", "colour", "green", "yellow", "orange", "fun" 
+						+ "one", "two", "three", "four", "five", "six", "seven", "eight", "excellent"};
+
+		System.out.print("Welcome to the anagram section! An anagram is a word that has its letters in a random order."
+				+ "\nYour job is to state the correct order of the word."
+				+ "\nCapitals are not used, so don't worry about having a lowercase at the beginning of a word! ");
+
+		//counts 10 questions
+		for (int z = 0; z<10; z++){
+			b = false;
+			//Makes sure words are not re-used
+			while(b == false){
+				b = true;
+				i = rnd.nextInt(25);//# of words-1
+				for(int y = 0; y<10; y++){
+					if(i == (test[y])){
+						b = false;
 					}
 				}
-				test[z]=i;
-				
-				System.out.print("Your word is: ");
-				//Sends the randomly picked word to be scrambled
-				scram(word[i]);
-				System.out.print("\nState the ordered word: ");
-				//Incase they put in a capital letter
-				ans = kb.nextLine().toLowerCase();
-				if (ans.equals(word[i])){
-					System.out.println("That is correct! ");
-					score1++;
-				}
-				else
-					System.out.println("This is incorrect.\nThe answer we were looking for was " + word[i]);
 			}
-				
-				
-				
-			return score1;
+			test[z]=i;
+
+			System.out.print("Your word is: ");
+			//Sends the randomly picked word to be scrambled
+			scram(word[i]);
+			System.out.print("\nState the ordered word: ");
+			//Incase they put in a capital letter
+			ans = kb.nextLine().toLowerCase();
+			if (ans.equals(word[i])){
+				System.out.println("That is correct! ");
+				score1++;
+			}
+			else
+				System.out.println("This is incorrect.\nThe answer we were looking for was " + word[i]);
 		}
-	
+
+
+
+		return score1;
+	}
+
 	public static void scram(String word){
-	//Word Scrambler
+		//Word Scrambler
 		Random rnd = new Random();
 		int length = word.length();
 		int [] sword = new int [length];
@@ -118,25 +120,49 @@ public class WordMaster {
 		}
 	}
 	public static int punc(){
-	//Have a sentence vault, choose 10 or so at random. (multi choice)
-	//Choose between . , ; ' and ?
-		
+		//Have a sentence vault, choose 10 or so at random. (multi choice)
+		//Choose between . , ; ' and ?
+
 		//First 20 will be . The next 20 will be , and so on
 		String [] question = {"The dog says woof[ ]", "The cat says meow[ ]", "I like cheese[ ]", "I went for a walk[ ]", "Hit the ball with the paddle[ ]"
-		+ "It's fun to run[ ]", "School is where you learn[ ]", "Don't run with scissors[ ]", "It is sunny outside today[ ]", "Hit the ball with the bat[ ]" //Periods
-		+ ""};
-		
-		
-		
-		
-		
-		return score2;
+				, "It's fun to run[ ]", "School is where you learn[ ]", "Don't run with scissors[ ]", "It is sunny outside today[ ]", "Hit the ball with the bat[ ]" //Periods
+				, ""};
+
+
+
+
+
+		return 0;
 	}
 	public static int homon(){
-	//Have a sentence vault, choose 10 or so at random. (multi choice)
-		
-		
-		
-		return score3;
+		//Have a sentence vault, choose 10 or so at random. (multi choice)
+
+
+
+		return 0;
+	}
+
+	public static int[] questionSelect(int length){
+		boolean b;
+		int i=0;
+
+		Random rnd=new Random();
+		int [] test = new int[10];
+		//counts 10 questions
+		for (int z = 0; z<10; z++){
+			b = false;
+			//Makes sure words are not re-used
+			while(b == false){
+				b = true;
+				i = rnd.nextInt(length);
+				for(int y = 0; y<z; y++){
+					if(i == (test[y])){
+						b = false;
+					}
+				}
+			}
+
+			test[z]=i;
+		} return test;
 	}
 }
